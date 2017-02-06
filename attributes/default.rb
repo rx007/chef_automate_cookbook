c06c84c788892ce35c335becb55efc450a0fd576
rex <rx007@me.com>
2017-02-06 15:27:27 +0900

    REALLY FUCKING FIXED

---
+++ attributes/default.rb
@@ -0,0 +1,2 @@
+default['chef_automate']['version'] = 'latest'
+default['chef_automate']['license'] = '/tmp/delivery.license'
--- recipes/default.rb
+++ recipes/default.rb
@@ -14,17 +14,32 @@ when 'rhel'
   end
 
 when 'debian'
+  package 'apt-transport-https' do
+    action :install
+  end
 
-  apt_repository 'nginx' do
+  apt_repository 'chef-stable' do
     uri          'https://packages.chef.io/repos/apt/stable'
+    components ['main']
     distribution node['lsb']['codename']
     deb_src      false
     key          'https://packages.chef.io/chef.asc'
   end
 
+  apt_update 'update trusty' do
+    action :update
+  end
 
 else
   log "Does not maintain packages for platform #{node['platform']}. Cannot setup the upstream repo!" do
     level :warn
   end
+end
+
+package 'delivery' do
+  action :install
+end
+
+execute 'automate-ctl_preflight_check' do
+  command '/usr/bin/automate-ctl preflight-check'
 end
